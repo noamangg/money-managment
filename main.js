b634d6571da38400589423998780e204af6656ca
Noaman <noamangg@gmail.com>
2023-08-27 19:43:14 +0300

    the project is done

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 let rightContainer = document.querySelector(".right");
 let icons = ['fa-clock', 'fa-sack-dollar', 'fa-code', 'fa-coins', 'fa-quote-left', 'fa-coins'];
 let h3Title = ['Hours Number', 'Total Amount', 'Coding Hours', 'Amount', 'Iqtibasy Project', 'Amount'];
+let jsonSt = '{"infoArray": [0,0,0,0,0,0]}';
+// localStorage.ProInfo = '{"infoArray": [0,0,0,0,0,0]}';
+let jsonOb = checkLocal();
 createBox();
+function checkLocal() {
+  if (!localStorage.ProInfo)
+    localStorage.ProInfo = jsonSt;
 
+  return JSON.parse(localStorage.ProInfo);
+}
 function createBox() {
   let div = document.createElement("div");
   div.classList.add('deatils-box');
@@ -32,7 +40,7 @@ function createBox() {
     else
       upSpanIcon.classList.add("fa-dollar-sign");
 
-      upSpanSpan.textContent = getInfoFromLocal(i);
+    upSpanSpan.textContent = getInfoFromLocal(i).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     rightContainer.appendChild(div.cloneNode(true));
 
     h3Icon.className = "fa-solid";
@@ -40,9 +48,40 @@ function createBox() {
   }
 
 }
-
 function getInfoFromLocal(index) {
-  
-  return "1000"
+  return jsonOb["infoArray"][index];
 }
 
+let inputFiled = document.querySelector("#record-inp");
+let selectCh = document.querySelector("#select-ob");
+let btn = document.querySelector("#add-btn");
+inputFiled.addEventListener("keypress", function(e) {
+  if(e.keyCode === 13) {
+    onclickBtn(e);
+  }
+})
+
+function onclickBtn(e) {
+
+  if (typeof +inputFiled.value === 'number' && selectCh.value !== '0') {
+    jsonOb["infoArray"][0] += +inputFiled.value;
+    jsonOb["infoArray"][1] = +jsonOb["infoArray"][0] * 3500;
+    if (selectCh.value === "coding") {
+      jsonOb["infoArray"][2] += +inputFiled.value;
+    }
+    else if (selectCh.value === "iqtibasy") {
+      jsonOb["infoArray"][4] += +inputFiled.value;
+
+    }
+    inputFiled.value = "";
+    jsonOb["infoArray"][3] = +jsonOb["infoArray"][2] * 3500;
+    jsonOb["infoArray"][5] = +jsonOb["infoArray"][4] * 3500;
+
+  }
+  updateLocal();
+}
+
+function updateLocal() {
+  localStorage.ProInfo = JSON.stringify(jsonOb);
+  location.reload();
+}
